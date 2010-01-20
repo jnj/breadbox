ee400fe47031dea71cc9ee2d767e12e098fa610c
Josh Joyce <jjoyce@drwholdings.com>
2010-01-19 23:09:17 -0600

    added mongo java driver to managed libs

--- project/build/ProjectConfig.scala
+++ project/build/ProjectConfig.scala
@@ -5,6 +5,7 @@ class ProjectConfig(info: ProjectInfo) extends DefaultWebProject(info) {
   val lift = "net.liftweb" % "lift-core" % "1.0.2" % "compile"
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test"
   val servlet = "javax.servlet" % "servlet-api" % "2.5" % "provided" 
+  val mongo = "org.mongodb" % "mongo-java-driver" % "1.2.1"
 
   // required because Ivy doesn't pull repositories from poms
   val smackRepo = "m2-repository-smack" at "http://maven.reucon.com/public"
